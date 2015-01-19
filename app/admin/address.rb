28bb0f20fab2f6c7461f9ebb71164b5d235a685e
Kizer <xyzkizer@gmail.com>
2015-01-19 16:58:50 +0800

    address index display customer name
    Changes to be committed:
          modified:   app/admin/address.rb

--- app/admin/address.rb
+++ app/admin/address.rb
@@ -10,12 +10,24 @@ ActiveAdmin.register Address do
   index do
     selectable_column
     id_column
+    column("Customer") {|address| address.customer.name}
     column :area
     column :address
     column :created_at
     actions
   end
 
+  # index do
+  #   selectable_column
+  #   column("Order", :sortable => :id) {|order| link_to "##{order.code} ", admin_order_path(order) }
+  #   column("State")                   {|order| status_tag(order.status) }
+  #   column("Payment")                 {|order| status_tag(order.payment) }
+  #   column("Delivery")                {|order| status_tag(order.delivery) }
+  #   column("Date", :created_at)
+  #   column("Customer", :customer, :sortable => :customer_id)
+  #   column("Total")                   {|order| number_to_currency order.total}
+  #   actions
+  # end
   #sidebar "Active Admin Demo" do
   #  render('/admin/sidebar_links', :model => 'users')
   #end
